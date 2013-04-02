d2a12840fa6e064f22791b3bc83f1a0204b13aaa
Andrew and Joe Myatt <habjoehighalt@gmail.com>
2013-04-02 18:14:45 +0200

    Update AURA-MEGA.cpp
    POLLS SENSORS GENERATES DATA STRUCTURE WHICH IS WRITTEN TO SD CARD AND SENT VIA I2C INTERFACE TO MOBILE MODULE AND TRANSMITTER

--- AURA-MEGA.cpp
+++ AURA-MEGA.cpp
@@ -1,18 +1,35 @@
 /*
 
     
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ HABJOE  Arduino Flight script
+ HABJOE - Copyright Andrew Myatt and Joseph Myatt March/2013
+ 
+ KEYWORDS: ARDUINO MEGA, HIGH ALTITUDE BALLOON, BMP085, ADXL345, UBLOX, I2C, WIRING, SDCARD
+ 
+ This code is in the public domain.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ 
+ This software is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ This software is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ POLLS SENSORS GENRATES DATA STRUCTURE WHICH IS WRITTEN TO SD CARD AND SENT VIA 
+ I2C INTERFACE TO MOBILE MODULE AND TRANSMITTER
+ 
 */
 #include <SPI.h>
 #include <RFM22.h>a
